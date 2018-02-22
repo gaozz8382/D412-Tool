8952fa27aab8a9723064e7459ee5e7e481404029
Gao zhengzhong <gaozz8382@163.com>
2018-02-22 21:18:59 +0800

    modify gui.py file

--- D412-Tool/gui_module/gui.py
+++ D412-Tool/gui_module/gui.py
@@ -4,35 +4,35 @@
 from tkinter import *
 import tkinter.messagebox as messagebox
 
-def gui_start():
-    init_window = Tk()
-    init_window.mainloop()
+# def gui_start():
+#     init_window = Tk()
+#     init_window.mainloop()
 
-gui_start()
+# gui_start()
 
-# class Application(Frame):
-#     def __init__(self, master=None):
-#         Frame.__init__(self, master)
-#         self.pack()
-#         self.createWidgets()
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
-#     def createWidgets(self):
-#         self.nameInput = Entry(self)
-#         self.nameInput.pack()
-#         self.alertButton = Button(self, text='START', command=self.hello)
-#         self.alertButton.pack()
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='START', command=self.hello)
+        self.alertButton.pack()
     
-#     def hello(self):
-#         # name = self.nameInput.get() or 'world'
-#         name = 'world'
-#         messagebox.showinfo('Message', 'Hello, %s' % name)
+    def hello(self):
+        # name = self.nameInput.get() or 'world'
+        name = 'world'
+        messagebox.showinfo('Message', 'Hello, %s' % name)
 
-# # 
-# app = Application()
-# # set window title：
-# app.master.title('D412-Tool')
-# app.createWidgets()
+# 
+app = Application()
+# set window title：
+app.master.title('D412-Tool')
+app.createWidgets()
 
-# # main fuction
-# if __name__ == '__main__':
-#     app.mainloop()
+# main fuction
+if __name__ == '__main__':
+    app.mainloop()
